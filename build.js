11652f693d522eb8353f9460556adaae37d572dd
astapovich <46627721+astapovich@users.noreply.github.com>
2022-11-01 13:18:01 +0300

    Update build.js

--- build.js
+++ build.js
@@ -85,7 +85,7 @@ function getStyleDictionaryConfig(theme) {
     ],
     "platforms": {
       "web": {
-        "transforms": ["attribute/cti", "name/cti/kebab", "sizes/px", "shadow/css", "innershadow/css"],
+        "transforms": ["attribute/cti", "name/cti/kebab", "sizes/px", "shadow/css", "innershadow/css", "color/rgba"],
         "buildPath": `output/`,
         "files": [{
             "destination": `${theme}.css`,
